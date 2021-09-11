2067e1ddb65d1d6c77f5cf7286105511fefb78f9
Sushant Sharma <ssushantlb08@gmail.com>
2021-09-12 03:06:38 +0530

    code: whileLoop_basic

---
+++ whileLoop.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()	
+{	
+    int a;
+    printf("Enter the value of a : ");
+    scanf("%d",&a);
+
+    while (a<11)
+    {
+        printf("%d\n",a);
+        a++;
+    }
+    
+    
+    return 0;
+}
