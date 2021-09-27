fa68204f42ef916dd71256ed8e39e8c64d54bea1
Sushant Sharma <ssushantlb08@gmail.com>
2021-09-27 20:58:40 +0530

    code:avgFunction

---
+++ function$/avgFunction.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int avg();
+int main()	
+{	
+    int a,b,c;
+    printf("Enter the value of a, b and c :");
+    scanf(" %D %d %d",&a,&b,&c);
+     avg()    
+    
+    return 0;
+}
+int avg(){
+       printf(" the avg value is : %d",(a+b+c)/3);
+}
