cba5795d191d88a9df8391da905c203fb5b51d99
Sushant Sharma <ssushantlb08@gmail.com>
2021-10-04 23:00:49 +0530

    code:refactor:basic

--- pyramids/FullPyramid.c
+++ pyramids/FullPyramid.c
@@ -6,7 +6,7 @@ int main()
     printf("Enter a value : ");
     scanf("%D",&n);
     printf("\n");
-    for ( i = 1; i <= n; i++)
+    for ( i = 1 ; i <= n; i++)
     {
         for (j = 1; j <=i; j++)
         {
