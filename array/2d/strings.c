3b4936765d64f3fe19af8fb468ecb3493853ae08
Sushant Sharma <88335357+sushantsharma08@users.noreply.github.com>
2021-11-03 22:57:28 +0530

    code:array
    string

---
+++ array/2d/strings.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()	
+{	
+    char str[10]="...name...";
+    printf("current name : %s\n",str);
+    printf("enter a name\n");
+    scanf("%s",str);
+    printf("name : %s \n ",str);
+    return 0;
+}
