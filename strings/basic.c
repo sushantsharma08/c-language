22ddd5abe676a8d17bbbbc7361c554ea47934eda
Sushant Sharma <88335357+sushantsharma08@users.noreply.github.com>
2021-11-11 22:02:18 +0530

    update : code : string
    basic

--- strings/basic.c
+++ strings/basic.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 int main()	
 {	
-    char str[]={'s','u','s','h','a','n','t','\0'};
-    printf(" string has value : %s",str);
+    char str1[]={'s','u','s','h','a','n','t','\0'};
+    char str2[]="sushant";
+    printf(" string one has value : %s\n",str1);
+    printf(" string two has value : %s\n",str2);
     
     return 0;
 }
