679ee71bed67a74ffe0869121de53d5a811bac83
Sushant Sharma <88335357+sushantsharma08@users.noreply.github.com>
2021-11-21 22:50:39 +0530

    refactor: structure

--- Structures/pointerToStructure.c
+++ Structures/pointerToStructure.c
@@ -13,7 +13,8 @@ int main()
     struct employee E1;  //this makes a variable E1
     struct employee *ptr;  // this point to a variable of structure named employee
     ptr=&E1;
-    (*ptr).code=101;
+    // (*ptr).code=101;
+    ptr->code=101;   // both are same shit
     printf("Code is %d",E1.code);
     
     return 0;
