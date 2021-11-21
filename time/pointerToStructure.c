60faca814844cb1c8f5bf2fe49513e15d1eb587f
Sushant Sharma <88335357+sushantsharma08@users.noreply.github.com>
2021-11-21 22:27:10 +0530

    Create pointerToStructure.c

---
+++ time/pointerToStructure.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include<string.h>
+
+struct employee
+{
+    float salary;
+    int code;
+    char name[20];
+    };
+
+int main()	
+{	
+    struct employee E1;  //this makes a variable E1
+    struct employee *ptr;  // this point to a variable of structure named employee
+    ptr=&E1;
+    (*ptr).code=101;
+    printf("Code is %d",E1.code);
+    
+    return 0;
+}
