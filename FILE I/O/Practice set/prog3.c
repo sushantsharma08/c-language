25e45a3b53e15c60c5746ceae36e1628bd507910
Sushant Sharma <88335357+sushantsharma08@users.noreply.github.com>
2021-11-23 01:44:00 +0530

    practiceProg3

---
+++ FILE I/O/Practice set/prog3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main()	
+{	
+    FILE *ptr, *ptr2;
+    char c, r;
+    ptr = fopen("prog3.txt", "r");
+    ptr2 = fopen("prog3_write.txt", "w");
+    c = fgetc(ptr);
+    while (c != EOF)
+    {
+        printf("%c",c);
+        fputc(c , ptr2);
+        c = fgetc(ptr);
+    }
+    // fflush(c);
+    // c = fgetc(ptr);
+    // while (c != EOF)
+    // {
+    //     fputc(c , ptr2);
+    //     c = fgetc(ptr);
+    // }
+    
+
+
+
+    return 0;
+}
